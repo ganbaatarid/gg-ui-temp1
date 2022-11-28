d3dc14ccfbcdc0c45f770d2ecc3352c544bfa229
ganbaatar <ganbaatarid@gmail.com>
2022-11-28 15:03:58 +0800

    g

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MyCounter, AddIcon, Button } from "gg-ui-temp1";
+import { MyCounter, Button } from "gg-ui-temp1";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <div>
-      <AddIcon />
       <Button />
     </div>
   </React.StrictMode>
