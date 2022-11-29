0ad0b98b8c2e17794d9e94d73440c305248e00f4
ganbaatar <ganbaatarid@gmail.com>
2022-11-29 09:59:07 +0800

    storybook

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import MyCounter from "./components/App";
-import Button from "./components/Button";
-export { Button };
+
 export { MyCounter };
 
 //icons
 // export { default as Icon } from './components/Icon/Icon'
 // export type { IconProps } from './components/Icon'
 
+// import Icon from "./components/Icon/IconsList/Add";
+// export { Icon };
 export { default as AddIcon } from "./icons/Add";
+
+import { Button } from "./components/Button";
+export { Button }
