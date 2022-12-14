82818eeb5fcaba88902ce4ae8f39bdf12004d8bd
ganbaatar <ganbaatarid@gmail.com>
2022-12-14 08:32:49 +0800

    g

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MyCounter, Button, AddIcon } from "gg-ui-temp1";
+import { Button, AddIcon } from "gg-ui-temp1";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
