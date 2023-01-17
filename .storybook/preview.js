599f46553c5cbb137c21547431575453c3a54e4c
ganbaatar <ganbaatarid@gmail.com>
2023-01-17 13:38:53 +0800

    g

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '../src/index.css';
+import '../src/tailwind.css';
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
